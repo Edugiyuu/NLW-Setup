e21037b7d5a523d55146fd7aa60b9db1d7a4b390
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2023-01-21 00:21:44 -0300

    Aula-2-Ignite

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css'
+
+//import { Habit } from "./components/Habit";
+
+export function App() {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl'></div>
+    </div>
+  )
+}
+

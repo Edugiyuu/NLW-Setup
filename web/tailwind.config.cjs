42af6bd03cc32fd17754be6caddf5438ca063d04
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2023-01-22 14:16:53 -0300

    Aula-2

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -7,7 +7,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: '#09090A'
+        background:'#09090A'
+      },
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0, 1fr))'
       },
     },
   },
